22ddc60e0025a7b39b72f551054ddb8ab47af4db
Anasou1 <anasoudsassi@gmail.com>
2024-04-10 14:23:11 +0000

    updating 2-then.js file

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  const pro = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject(Error());
-    }
-  }).then(() => console.log('Got a response from the API'));
-  return pro;
+  return promise.then(() => console.log('Got a response from the API'));
 }
