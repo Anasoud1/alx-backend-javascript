7a36ea7678e9c4df7800268ea38b3c319042279c
Anasou1 <anasoudsassi@gmail.com>
2024-04-15 17:30:52 +0100

    adding -clean_set.js file

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, string) {
+  if (string === '') return '';
+  const list = [...set].filter((element) => element.substring(0, string.length) === string);
+  const arr = [];
+  list.forEach((item) => arr.push(item.substring(string.length)));
+  return arr.join('-');
+}
