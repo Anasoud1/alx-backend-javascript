853b21230c2cff6930009adb89a76519fb3dc00a
Anasou1 <anasoudsassi@gmail.com>
2024-04-16 13:22:13 +0100

    updating 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  const list = [...set].filter((el) => el.substring(0, startString.length) === startString);
+  const list = [...set].filter((el) => typeof el === 'string' && el.substring(0, startString.length) === startString);
   const arr = [];
   list.forEach((item) => arr.push(item.substring(startString.length)));
   return arr.join('-');
