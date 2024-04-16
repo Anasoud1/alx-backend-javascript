1c2720024e367e0e6e945a010e160ab067093ce5
Anasou1 <anasoudsassi@gmail.com>
2024-04-16 13:57:59 +0100

    updating  100-weak.js file

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, count + 1);
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 
-  if (count >= 5) throw new Error('Endpoint load is high');
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
