1327d1f6babb9c91ea7eccf8442ef6270ab606e8
Anasou1 <anasoudsassi@gmail.com>
2024-04-17 17:42:59 +0100

    adding StudentClass class to js/main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,28 @@ export interface printTeacherFunction {
 export const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`
 };
+
+export interface StudentClassConstructor{
+  new(firstName: string, lastName: string): StClass;
+}
+
+export interface StClass{
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StClass{
+  private _firstName!: string;
+  private _lastName!: string;
+  constructor(firstName: string, lastName: string){
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this._firstName;
+  }
+}
